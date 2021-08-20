ddaef362d6a0f5d02e480ddf0347167cf93455e4
Antonio Neto <prof.antonioqsneto@gmail.com>
2021-08-20 00:04:29 -0300

    Create delete viws

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,30 @@
 from django.conf.urls import url, include
-from .views import (home, 
-                    lista_pessoas, 
-                    lista_veiculos, 
-                    listaMovRot, 
+from .views import (home,
+                    lista_pessoas,
+                    lista_veiculos,
+                    listaMovRot,
                     pessoa_novo,
                     veiculo_novo,
-                    movrotativo_novo)
+                    movrotativo_novo,
+                    veiculo_update,
+                    movrotativos_update,
+                    pessoa_delete)
 
 urlpatterns = [
     url(r'^$', home, name="core_home"),
     url(r"^pessoas/$", lista_pessoas, name="core_lista_pessoas"),
     url(r"^pessoas-novo/$", pessoa_novo, name="core_pessoas_novo"),
+    url(r'pessoa-delete/(?P<id>\d+)/$',
+        pessoa_delete, name='core_pessoa_delete'),
+
     url(r"^veiculos/$", lista_veiculos, name="core_lista_veiculos"),
     url(r"^veiculo-novo/$", veiculo_novo, name="core_veiculo_novo"),
+    url(r'veiculo-update/(?P<id>\d+)/$',
+        veiculo_update, name='core_veiculo_update'),
+
     url(r"^movrotativo/$", listaMovRot, name="core_lista_movrot"),
-    url(r"^movrotativo-novo/$", movrotativo_novo, name="core_novorotativo_movrot")
+    url(r"^movrotativo-novo/$", movrotativo_novo,
+        name="core_novorotativo_movrot"),
+    url(r'mov_rot_update/(?P<id>\d+)/$',
+        movrotativos_update, name='core_movrotativos_update')
 ]
--- core/views.py
+++ core/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 from .form import PessoaForm, VeiculoForm, MovRotativoForm
 
 
-
 def home(request):
     return HttpResponse('ola mundo')
 
@@ -22,6 +21,15 @@ def pessoa_novo(request):
     return redirect("core_lista_pessoas")
 
 
+def pessoa_delete(request, id):
+    pessoa = Pessoa.objects.get(id=id)
+    if request.method == 'POST':
+        pessoa.delete()
+        return redirect('core_lista_pessoas')
+    else:
+        return render(request, 'core/delete_confirm.html', {'pessoa': pessoa})
+
+
 def lista_veiculos(request):
     veiculos = Veiculo.objects.all()
     form = VeiculoForm()
@@ -36,14 +44,45 @@ def veiculo_novo(request):
     return redirect("core_lista_veiculos")
 
 
+def veiculo_update(request, id):
+    data = {}
+    veiculo = Veiculo.objects.get(id=id)
+    form = VeiculoForm(request.POST or None, instance=veiculo)
+    data['veiculo'] = veiculo
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('core_lista_veiculos')
+    else:
+        return render(request, 'core/update_veiculo.html', data)
+
+
 def listaMovRot(request):
     movrot = MovRotativo.objects.all()
     form = MovRotativoForm()
     data = {'movrot': movrot, 'form': form}
     return render(request, 'core/lista_movRotativo.html', data)
 
+
 def movrotativo_novo(request):
     form = MovRotativoForm(request.POST or None)
     if form.is_valid():
         form.save()
     return redirect("core_lista_movrot")
+
+
+def movrotativos_update(request, id):
+    data = {}
+    mov_rotativo = MovRotativo.objects.get(id=id)
+    form = MovRotativoForm(request.POST or None, instance=mov_rotativo)
+    data['mov_rotativo'] = mov_rotativo
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('core_lista_movrot')
+    else:
+        return render(request, 'core/update_movrotativos.html', data)
